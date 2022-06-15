5b8c86d11679e3424f77d895fc45bbb06a683d1b
cezevogue <cezdesaulle.evogue@gmail.com>
2022-06-15 17:15:44 +0200

    2eme commit

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
+use App\Form\ProduitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,12 +15,23 @@ class AdminController extends AbstractController
     /**
     *@Route("/ajoutProduit", name="ajoutProduit") 
     */
-    public function ajoutProduit()
+    public function ajoutProduit(Request $request)
     {
+
+        $produit=new Produit();
+
+        $form= $this->createForm(ProduitType::class, $produit);
+
+        $form->handleRequest($request);
+
      
-        
+
      
-     return $this->render("admin/ajoutProduit.html.twig",[] ) ;
+     return $this->render("admin/ajoutProduit.html.twig",[
+        'form'=>$form->createView()
+
+
+     ] ) ;
     }
 
 
--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\Produit;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,13 +17,14 @@ class ProduitType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('prix')
-            ->add('photo1')
-            ->add('photo2')
-            ->add('photo3')
-            ->add('titre')
-            ->add('descriptif')
-            ->add('stock')
+            ->add('prix', NumberType::class)
+            ->add('photo1' , FileType::class)
+            ->add('photo2' , FileType::class)
+            ->add('photo3' , FileType::class)
+            ->add('titre', TextType::class)
+            ->add('descriptif', TextareaType::class)
+            ->add('stock', NumberType::class)
+            ->add('Ajouter', SubmitType::class)
         ;
     }
 
